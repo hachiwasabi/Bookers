d073f97580368d521412ea71f48fe35ab8f3e5ea
hachiwasabi <224736665+hachiwasabi@users.noreply.github.com>
2025-08-18 15:18:00 +0000

    [progress]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,10 +7,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to books_path, notice: 'Book was successfully created.'
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(@book)
     else
       @books = Book.all
-      render :index
+      flash[:notice] = 'Book was error created.'
+      render :new
     end
   end
 
@@ -30,7 +32,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "更新に成功しました！"
+      redirect_to book_path(@book), notice: "Book was successfully update."
     else
       render :edit
     end
